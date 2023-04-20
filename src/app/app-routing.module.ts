48504f5af1b4035a70113bc932c291ccee4515b0
SalyOne <salome.batkhadze@gmail.com>
2023-04-21 00:30:46 +0400

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from "./main-layout/main-layout.component";
+import {CashGamesComponent} from "./pages/cash-games/cash-games.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'cash-games',
+        component: CashGamesComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CashGamesComponent } from './pages/cash-games/cash-games.component';
+import { SpringSeriesComponent } from './pages/spring-series/spring-series.component';
+import { FinalStageComponent } from './pages/final-stage/final-stage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CashGamesComponent,
+    SpringSeriesComponent,
+    FinalStageComponent
   ],
   imports: [
     BrowserModule,
